be4a5a088a2d45847c5542dd744486d0fcd87625
David Berghoff <berghoff.david@gmail.com>
2022-02-11 20:44:25 -0600

    tested

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import AppContext from './contexts';
 
 export default function App() {
+  axios.post('/products')
+    .then((res) => {
+      console.log(res);
+    });
   return (
     // <AppContext>
     <div>
