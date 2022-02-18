d4a165f089a07fc35f7061baa155490d858821cb
Dennis Berezin <dennis.m.berezin@gmail.com>
2022-02-17 17:07:00 -0700

    Refactored CSS + Implemented most of Carousel

--- src/components/PD/PDCarouselSlides.jsx
+++ src/components/PD/PDCarouselSlides.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import contexts from '../contexts';
 import PDCarouselSlide from './PDCarouselSlide';
 
-export default function PDCarouselSlides() {
+export default function PDCarouselSlides({
+  currentPhoto, setCurrentPhoto,
+}) {
   const [currentStyle, setCurrentStyle] = React.useContext(contexts.DetailsContext);
   return (
-    <div>
-      {currentStyle.photos.map((photo) => (<PDCarouselSlide thisPhoto={photo} />))}
+    <div className="PDSlides">
+      {currentStyle.photos.map((photo) => (
+        <PDCarouselSlide
+          key={Math.random()}
+          currentPhoto={currentPhoto}
+          setCurrentPhoto={setCurrentPhoto}
+          thisPhoto={photo}
+        />
+      ))}
     </div>
   );
 }
+
+PDCarouselSlides.propTypes = {
+  currentPhoto: PropTypes.string,
+  setCurrentPhoto: PropTypes.func,
+};
