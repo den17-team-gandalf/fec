31914dc7fd425120cc04ca8528e85809c69ddab5
David Berghoff <berghoff.david@gmail.com>
2022-02-17 18:24:20 -0600

    implemented all features of rating breakdown, besides click to filter

--- src/components/ratingsAndReviews/ReviewStarBar.jsx
+++ src/components/ratingsAndReviews/ReviewStarBar.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import propTypes from 'prop-types';
 
 export default function ReviewStarBar({ numStars, modeStarRating, numMatching }) {
   return (
-    <div id={`StarsBar${numStars}`}>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div id={`StarsBar${numStars}`} onClick={() => console.log('Clicked')}>
       {`${numStars} stars`}
       <progress className="ReviewsRatingBar" max={modeStarRating} value={numMatching} />
       {numMatching}
