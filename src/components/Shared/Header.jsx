e441941452d6ba7764985dfa40a0660c87627745
David Berghoff <berghoff.david@gmail.com>
2022-02-22 11:07:32 -0600

    small changes to make Headers not take a prop

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -1,62 +1,53 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faLightbulb } from '@fortawesome/free-solid-svg-icons';
-import PropTypes from 'prop-types';
 import contexts from '../contexts';
 
-export default function Header({ products }) {
+export default function Header() {
   const [darkMode, setDarkMode] = React.useState(false);
-  const [productNames, setProductNames] = React.useState([]);
   const [searching, setSearching] = React.useState('');
-  React.useEffect(() => {
-    // eslint-disable-next-line prefer-const
-    let pNames = [];
-    for (let i = 0; i < products.length; i++) {
-      pNames.push(products[i].name);
-    }
-    setProductNames(pNames);
-  }, [products]);
-  const selectChanger = (e) => {
-    setSearching(e.target.value);
-  };
-  const modeToggle = () => {
-    document.documentElement.setAttribute('data-theme', darkMode ? 'light' : 'dark');
-    setDarkMode(!darkMode);
-  };
   return (
-    <header className="header">
-      <span className="hLogo">
-        <strong>Project Catwalk</strong>
-        {' '}
-        - Team Gandalf -
-        {' '}
-        {' '}
-        {' '}
-        {' '}
-        <FontAwesomeIcon onClick={() => modeToggle()} className="I_bulb" icon={faLightbulb} />
-        <span className="hSearch">
-          <select className="hInput" type="text" name="size" id="size" onChange={(e) => selectChanger(e)}>
-            <option value="">Choose a Product...</option>
-            {productNames
-              .map(
-                (productName) => (
-                  <option
-                    key={productName}
-                    value={productName}
-                  >
-                    {productName}
-                  </option>
-                ),
-              )}
-          </select>
-          {/* <FontAwesomeIcon className="I_headerSGlass" icon={faMagnifyingGlass} /> */}
-        </span>
-      </span>
-
-    </header>
+    <contexts.AppContext.Consumer>
+      {(products) => {
+        const selectChanger = (e) => {
+          setSearching(e.target.value);
+        };
+        const modeToggle = () => {
+          document.documentElement.setAttribute('data-theme', darkMode ? 'light' : 'dark');
+          setDarkMode(!darkMode);
+        };
+        return (
+          <header className="header">
+            <span className="hLogo">
+              <strong>Project Catwalk</strong>
+              {' '}
+              - Team Gandalf -
+              {' '}
+              {' '}
+              {' '}
+              {' '}
+              <FontAwesomeIcon onClick={() => modeToggle()} className="I_bulb" icon={faLightbulb} />
+              <span className="hSearch">
+                <select className="hInput" type="text" name="size" id="size" onChange={(e) => selectChanger(e)}>
+                  <option value="">Choose a Product...</option>
+                  {products
+                    .map(
+                      ({ name }) => (
+                        <option
+                          key={name}
+                          value={name}
+                        >
+                          {name}
+                        </option>
+                      ),
+                    )}
+                </select>
+                {/* <FontAwesomeIcon className="I_headerSGlass" icon={faMagnifyingGlass} /> */}
+              </span>
+            </span>
+          </header>
+        );
+      }}
+    </contexts.AppContext.Consumer>
   );
 }
-
-Header.propTypes = {
-  products: PropTypes.object,
-};
